38555acd18873164f72cea4a0048a0d95210f154
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-03-08 02:41:11 +0300

    Added map and get data from api in map page

--- src/pages/Home/Map.tsx
+++ src/pages/Home/Map.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import MapChart from '../../components/MapChart'
+
+import WorldMap from "react-svg-worldmap";
+
+import { Grid, Box } from '@mui/material';
+
+import { Request } from '../../helpers/Request';
+
+import { ResponseProp } from '../../helpers/interface';
 
 function Map() {
+
+  // useState
+  const [selectedCountry, setSelectCountry] = useState<string>("");
+  const [statistics, setStatistics] = useState<ResponseProp[]>([])
+
+  // useEffect
+  useEffect(() => {
+      getCountriesValues()
+  }, [selectedCountry])
+
+
+  // Functions
+  const getCountriesValues = async() => {
+      const country = selectedCountry.toLocaleLowerCase().replace(' ', '-')
+      const result = await Request({
+          method: 'GET',
+          url: '/statistics?country=' + country 
+      })
+      setStatistics(result.response)
+  }
+
+  const data = [
+    { country: "tr", value: 85561976 },
+  ];
+
+
   return (
-    <div>Map</div>
+    <React.Fragment>
+      <Grid container>
+        <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+          <Box>
+              <WorldMap
+                onClickFunction={(countryName) => {
+                  console.log(countryName)
+                  setSelectCountry(countryName.countryName)
+                }}
+                color="red"
+                value-suffix="people"
+                size="xxl"
+                data={data}
+              />
+          </Box>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   )
 }
 
---
+++ src/pages/page.d.ts
@@ -0,0 +1,4 @@
+export interface MapValueProps { 
+     country: string, 
+     value: number
+}
