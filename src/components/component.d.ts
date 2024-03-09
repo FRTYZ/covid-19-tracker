dcb6608a71037f3661eb804ea022b3cb3fc6b8d3
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-03-10 02:56:28 +0300

    Removed SnackbarAlert component in project

--- src/components/component.d.ts
+++ src/components/component.d.ts
@@ -1,19 +1,13 @@
 import { TodoProps, TagsProps } from "../pages/Todo/todo.interface"
 
-// Other components
-export type snackbarOptionsProps = {
-    message?: string,
-    type?: string
-}
-
-export type snackBarProps = {
-    snackbarOptions: snackbarOptionsProps
-}
-
 export type CardSectionProp = {
     title?: string, 
     value?: number | string,
     status?: string,
     color?: string,
     bottomText?: string
+}
+
+export type ChartLoadProps = {
+    variant?: string
 }
